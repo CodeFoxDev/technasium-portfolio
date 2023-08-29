f2969535d4fc3976be493ea0a3c2b6c7941dc7c0
Codefoxdev <admin@codefoxdev.com>
2023-08-29 19:39:45 +0200

    Added a front grid with highlighted projects

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import config from "/src/site.config.js";
+import config from "./site.config.js";
 
 const nav = document.querySelector(".nav-progress");
 const navTop = document.querySelector(".nav-top");
 const projects = document.querySelector("div.projects");
 const frontGrid = document.querySelector("#front-grid");
+const frontImgGrid = document.querySelector("#images-grid");
 
 document.addEventListener("scroll", updateProgress);
 
@@ -39,6 +40,7 @@ function updateProgress() {
   });
 
   updateProgress();
+  updateFrontGrid();
 })();
 
 function getButtons(project) {
@@ -52,3 +54,49 @@ function getButtons(project) {
 }
 
 // TODO: Front grid effects
+function updateFrontGrid() {
+  const tiles = Array.from(frontGrid.querySelectorAll("rect[--data-project]"));
+  tiles.sort((a, b) => {
+    const bounds = [ a.getBoundingClientRect(), b.getBoundingClientRect() ];
+    const area = [ bounds[0].width * bounds[0].height, bounds[1].width * bounds[1].height ];
+    if (area[0] > area[1]) return -1;
+    if (area[0] < area[1]) return 1;
+    return 0
+  });
+  
+  const projectIndices = generateRandomIndices(0, config.projects.length, tiles.length);
+  console.log(projectIndices);
+  tiles.forEach((e, i) => {
+    if (i > config.projects.length - 1) return;
+    const ele = document.createElement("div");
+    const scrollUpdate = () => {
+      const bounds = e.getBoundingClientRect();
+      ele.style.setProperty("--width", `${bounds.width - 2}px`);
+      ele.style.setProperty("--height", `${bounds.height - 2}px`);
+      ele.style.top = `${bounds.top + window.scrollY + 1}px`;
+      ele.style.left = `${bounds.left}px`;
+    }
+    ele.className = "front-image-grid";
+    ele.style.backgroundImage = `url("${config.projects[projectIndices[i]].src}")`;
+
+    scrollUpdate();
+    addEventListener("resize", scrollUpdate)
+
+    frontImgGrid.append(ele);
+    tiles[i].dataset.tileHasImage = ""
+  })
+}
+
+function generateRandomIndices(min, max, count) {
+  const _count = Math.min(max, count);
+  let _res = [];
+
+  for (let i = 0; i < _count; i++) rNum(_res, min, max);
+  return _res;
+}
+
+function rNum (res, min, max) {
+  let _num = Math.round(min + Math.random() * (max - min - 1));
+  if (res.indexOf(_num) == -1) return res.push(_num);
+  return rNum(res, min, max);
+}
